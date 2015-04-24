bcd7d018eb8184f841ec09005e8a15c79de3e453
djvoa12 <djvoa12@gmail.com>
2015-04-23 19:44:41 -0700

    corrected filters for timezone offset

--- frontend/app/pods/index/controller.js
+++ frontend/app/pods/index/controller.js
@@ -6,7 +6,7 @@ export default Ember.Controller.extend({
   upcomingEvents: computed.filter('model', (event) => {
     var date = new Date();
     date.setDate(date.getDate() - 1);
-    date.setHours(22);
+    date.setHours(date.getHours() - 7);
 
     if (date < event.get('date')) {
       return event;
